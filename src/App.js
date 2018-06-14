02b92a7ed0898fa049d5147e0ec81280d9c74385
nori007 <nori007@yettiesoft.com>
2018-06-14 13:46:33 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2> Stand Up 시작합시다. </h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          프로젝트 시작.
         </p>
       </div>
     );
